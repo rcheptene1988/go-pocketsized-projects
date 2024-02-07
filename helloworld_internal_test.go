ea6f5947a2f06b8de2e52873cc61a1c4427c06f4
rcheptene1988 <54058792+rcheptene1988@users.noreply.github.com>
2024-02-07 14:52:53 +0000

    testing improvements

--- helloworld_internal_test.go
+++ helloworld_internal_test.go
@@ -3,10 +3,51 @@ package main
 import "testing"
 
 func TestGreet(t *testing.T) {
-	expectedGreeting := "Hello world"
-	greeting := greet()
-	if greeting != expectedGreeting {
-		// mark this test as failed
-		t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
+	type testCase struct {
+		lang             language
+		expectedGreeting string
+	}
+	var tests = map[string]testCase{
+		"English": {
+			lang:             "en",
+			expectedGreeting: "Hello world",
+		},
+		"French": {
+			lang:             "fr",
+			expectedGreeting: "Bonjour le monde",
+		},
+		"Akkadian, not supported": {
+			lang:             "akk",
+			expectedGreeting: `unsupported language: "akk"`,
+		},
+		"Greek": {
+			lang:             "el",
+			expectedGreeting: "Χαίρετε Κόσμε",
+		},
+		"Hebrew": {
+			lang:             "he",
+			expectedGreeting: " שלום עולם ",
+		},
+		"Urdu": {
+			lang:             "ur",
+			expectedGreeting: " ہیلو دنیا ",
+		},
+		"Vietnamese": {
+			lang:             "vi",
+			expectedGreeting: "Xin chào Thế Giới",
+		},
+		"Empty": {
+			lang:             "",
+			expectedGreeting: `unsupported language: ""`,
+		},
+	}
+	// range over all the scenarios
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			greeting := greet(tc.lang)
+			if greeting != tc.expectedGreeting {
+				t.Errorf("expected: %q, got: %q", tc.expectedGreeting, greeting)
+			}
+		})
 	}
 }
--- main.go
+++ main.go
@@ -3,10 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	greeting := greet()
+	greeting := greet("hh")
 	fmt.Println(greeting)
 }
 
-func greet() string {
-	return "Hello world"
+// language represents the language’s code
+type language string
+
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": " שלום עולם ",       // Hebrew
+	"ur": " ہیلو دنیا ",       // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+}
+
+// greet says hello to the world in the specified language
+func greet(l language) string {
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
+	}
+	return greeting
 }
